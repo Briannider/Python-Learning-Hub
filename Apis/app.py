b8c1bf0cdf2d290b889e20e2d9df4cd7cffa5cee
Brian Carballo <braianniderhaus@gmail.com>
2024-01-31 00:15:44 -0300

    update: using pokemon api

--- Apis/app.py
+++ Apis/app.py
@@ -2,14 +2,29 @@
 Esta aplicacion trata de utilizar una Api
 """
 import requests
-
-URL = "https://jsonplaceholder.typicode.com/users/3"
+import json
+URL = "https://pokeapi.co/api/v2/pokemon/ditto"
 response = requests.get(URL, timeout=10)
 
 if response.status_code == 200:
     print('Solicitud exitosa')
     data = response.json()
-    for llave, valor in data.items():
-        print(llave, valor)
+    id = data['id']
+    name = data['name']
+    height = data['height']
+    weight = data['weight']
+    mensaje = f"""
+    id: {id}
+    name: {name}
+    height: {height}
+    weight: {weight}
+    """
+    print(mensaje)
+
+    # print(json.dumps(data, indent=2))
+    # for ability in data["abilities"]:
+    #     for clave, valor in ability.items():
+    #         print(f"{clave}: {valor}")
+
 else:
     print('Error en la solicitud, detalles:', response.text)
