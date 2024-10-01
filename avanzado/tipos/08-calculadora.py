f4526e6750b4fc2d6396ea398486c7659ac9916e
Brian Carballo <braianniderhaus@gmail.com>
2024-10-01 19:16:40 -0300

    fix: :bug: division by zero
    nothing else
    
    - division by zero

--- avanzado/tipos/08-calculadora.py
+++ avanzado/tipos/08-calculadora.py
@@ -4,13 +4,13 @@ n2 = int(input("Ingrese segundo numero: "))
 suma = n1 + n2
 resta = n1 - n2
 multi = n1 * n2
-div = n1 / n2
-pot = n1 ** n2
+div = n1 / n2 if n2 != 0 else "No se puede dividir por 0"
+pot = n1**n2
 
 mensaje = f"""
 Para los numeros {n1} y {n2}:
 la suma es = {suma}
-la resta es = {resta}
+la resta es = {resta}2
 la multiplicacion es = {multi}
 la division es = {div}
 la potencia es = {pot}
