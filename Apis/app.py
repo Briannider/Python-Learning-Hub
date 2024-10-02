6c4cc5b9710c39786eb770ccc56e86904939d8f1
Brian Carballo <braianniderhaus@gmail.com>
2024-10-01 21:59:10 -0300

    add nothing diferent

--- Apis/app.py
+++ Apis/app.py
@@ -1,9 +1,11 @@
 """
-Esta aplicacion trata de utilizar una Api
+Esta aplicacion trata de utilizar una Api la misma requiere de la libreria requests como se puede ver en el archivo requirements.txt
+la misma realiza una peticion GET a la url https://pokeapi.co/api/v2/pokemon/ditto y la imprime en consola
 """
 
 import requests
-import json
+
+# import json
 
 URL = "https://pokeapi.co/api/v2/pokemon/ditto"
 response = requests.get(URL, timeout=10)
@@ -11,6 +13,5 @@ response = requests.get(URL, timeout=10)
 if response.status_code == 200:
     print("Solicitud exitosa")
     data = response.json()
-
 else:
     print("Error en la solicitud, detalles:", response.text)
