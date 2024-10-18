803d2145634db4bc219f3fe6bf09bcbca7607b0b
Brian Carballo <braianniderhaus@gmail.com>
2024-10-18 18:55:22 -0300

    updated: lists

--- Theory/advanced/advanced_types/01-lists.py
+++ Theory/advanced/advanced_types/01-lists.py
@@ -1,20 +1,24 @@
-numeros = [1, 2, 3]
-letras = ["a", "b", "c"]
-palabras = ["Chanchito", "Feliz"]
-palabrasFelices = [palabras, "Felipe", "Alumno"]
-booleans = [True, False, True, False]
-matriz = [[0, 1], [1, 0]]
+# Lists are ordered collections of items that can be of any data type, including strings, integers, floats, and other lists.
+# Lists are defined using square brackets and elements are separated by commas.
+# Lists are mutable, meaning they can be changed after they are created.
 
-ceros = [0] * 10  # => Creacion de listado automatico de valores en 10 celdas
-# => Creacion de listado automatico de valores en 10 celdas
-ceros_unos = [0, 1] * 10
-# print(ceros)
-# print(ceros_unos)
+numbers = [1, 2, 3]  # => List of integers
+letters = ["a", "b", "c"]  # => List of strings
+words = ["Happy", "little pig"]  # => List of strings
+happyWords = [words, "Felipe", "Student"]  # => List of lists and strings
+booleans = [True, False, True, False]  # => List of booleans
+matrix = [[0, 1], [1, 0]]  # => List of lists (matrix)
 
-alfanumerico = numeros + letras
+# Automatic creation of a list with 10 cells filled with zeros
+zeros = [0] * 10
+# Automatic creation of a list alternating zeros and ones 10 times
+zeros_ones = [0, 1] * 10
 
+# Concatenation of lists
+alphanumeric = numbers + letters
 
-rango = list(range(1, 11))
-chars = list("Hola mundo")
+# List comprehension
+range_list = list(range(1, 11))
+characters = list("Hello world")
 
-print(chars)
+print(characters)
