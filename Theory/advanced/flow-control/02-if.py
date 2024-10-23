b6d8a40132c3d57151f522561e36326807bf1d86
Brian Carballo <braianniderhaus@gmail.com>
2024-10-22 22:51:02 -0300

    fix:  little issues

--- Theory/advanced/flow-control/02-if.py
+++ Theory/advanced/flow-control/02-if.py
@@ -1,8 +1,26 @@
-edad = 18
+age = 18
 
-if edad >= 18:
-    print("El usuario cumple la edad")
-elif edad < 18:
-    print("El usuario no cumple la edad")
-else:
-    print("Vete a otro lado amiguito")
+
+def check_age_requirement(age):
+    """
+    Check if the user meets the age requirement.
+
+    Parameters:
+    age (int): The age of the user.
+
+    Returns:
+    None
+    """
+    # Check if the age is 18 or older
+    if age >= 18:
+        print("The user meets the age requirement")
+    # Check if the age is less than 18
+    elif age < 18:
+        print("The user does not meet the age requirement")
+    # This else block will never be executed with the current conditions
+    else:
+        print("Go away, little friend")
+
+
+# Call the function to check the age requirement
+check_age_requirement(age)
