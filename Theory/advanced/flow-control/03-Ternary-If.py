8b844cf103534feb357ddc4d344ae1c160bdada9
Brian Carballo <braianniderhaus@gmail.com>
2024-10-23 09:46:38 -0300

    add: ternary if

--- Theory/advanced/flow-control/03-Ternary-If.py
+++ Theory/advanced/flow-control/03-Ternary-If.py
@@ -1,10 +1,25 @@
-edad = 18
+"""
+Example of using a ternary operator to assign a value to a variable
+based on a condition.
 
-mensaje = "Es mayor" if edad > 17 else "Es menor"
+The ternary operator is a shorthand way of writing a simple if-else
+statement.
 
-# if edad < 17 :
-#     mensaje = "Es mayor"
+In this example, the variable 'message' is assigned the string "Is
+older" if the variable 'age' is greater than 17, or "Is younger" if
+it is not.
+
+"""
+
+age = 18
+
+# Use the ternary operator to assign a value to the variable 'message'
+message = "Is older" if age > 17 else "Is younger"
+
+# The same code written with an if-else statement
+# if age < 17 :
+#     message = "Is older"
 # else:
-#     mensaje = "Es menor"
+#     message = "Is younger"
 
-print(mensaje)
+print(message)
