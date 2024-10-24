59f8c9309fe0ca66a217d846c2d4888971bf6282
Brian Carballo <84468272+Briannider@users.noreply.github.com>
2024-10-24 08:58:16 -0300

    Update 04-string-methods.py

--- Theory/advanced/types/04-string-methods.py
+++ Theory/advanced/types/04-string-methods.py
@@ -1,4 +1,4 @@
-animal = " Chanchito Feliz! "
+animal = " Happy little pig"
 print(animal.upper())
 print(animal.lower())
 print(animal.strip().capitalize())
@@ -8,7 +8,7 @@ print(animal.lstrip())
 print(animal.rstrip())
 print(animal.casefold())
 print(animal.__sizeof__())
-# .find() busca el string en la cadena y el Nro que retorna es la posicion
+# .find() Search on the string and then return the position in int
 # Retorna -1 si no se encuentra dentro de la cadena
 print(animal.find("ch"))
 print(animal.find("rh"))
