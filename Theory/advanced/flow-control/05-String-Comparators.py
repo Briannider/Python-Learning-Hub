cf6ae63be9c5ef843b63bae0dd58dee33a33e603
Brian Carballo <braianniderhaus@gmail.com>
2024-10-25 22:36:47 -0300

    add: 05 strings

--- Theory/advanced/flow-control/05-String-Comparators.py
+++ Theory/advanced/flow-control/05-String-Comparators.py
@@ -1,4 +1,17 @@
-edad = 25
+"""
+String Comparators in Python
 
-if 15 <= edad <= 65:
-    print("Puede entrar a la piscina")  # Es bellisimo d=====(￣▽￣*)b
+In this example, we will see how to use string comparators in Python.
+
+String comparators are used to compare strings in Python.
+
+In this example, we will compare the value of the variable "age" with the string "15" and "65".
+"""
+
+# Define the variable age
+age = 25
+
+# Check if the value of age is between 15 and 65
+if 15 <= age <= 65:
+    # Print a message if the condition is true
+    print("You can enter the pool")  # It's beautiful d=====(￣▽￣*)b
